23ab350ba334cf3ef7035a6ec6c17e8fe29cc6a9
Anton Holub <golubanton753@gmail.com>
2022-12-19 17:17:51 +0300

    feat: apply swiper carousel

--- src/components/Main/MainSlider/MainSlider.js
+++ src/components/Main/MainSlider/MainSlider.js
@@ -1,19 +1,51 @@
-import { Carousel } from "react-responsive-carousel";
 import MainSlide from "../MainSlide/MainSlide";
+import {  Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 const MainSlider = () => {
     return ( 
-        <Carousel showArrows={false}>
-            <MainSlide
-                subtitle='Best Furniture For Your Castle....'
-                title='New Furniture Collection Trends in 2020'
-                text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est adipiscing in phasellus non in justo.'
-                button='Shop Now'
-                img='./images/armchair.png'
-                alt='armchair'
-                sale='50% off'
-            />
-        </Carousel>
+        <Swiper
+            modules={[Pagination]}
+            pagination={{ clickable: true }}
+            slidesPerView={1}
+            spaceBetween={100}
+        >
+            <SwiperSlide>
+                <MainSlide
+                        subtitle='Best Furniture For Your Castle....'
+                        title='New Furniture Collection Trends in 2020'
+                        text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est adipiscing in phasellus non in justo.'
+                        button='Shop Now'
+                        img='./images/armchair.png'
+                        alt='armchair'
+                        sale='50% off'
+                />
+            </SwiperSlide>
+            <SwiperSlide>
+                <MainSlide
+                        subtitle='Best Furniture For Your Castle....'
+                        title='New Furniture Collection Trends in 2020'
+                        text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est adipiscing in phasellus non in justo.'
+                        button='Shop Now'
+                        img='./images/armchair.png'
+                        alt='armchair'
+                        sale='50% off'
+                />
+            </SwiperSlide>
+            <SwiperSlide>
+                <MainSlide
+                        subtitle='Best Furniture For Your Castle....'
+                        title='New Furniture Collection Trends in 2020'
+                        text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est adipiscing in phasellus non in justo.'
+                        button='Shop Now'
+                        img='./images/armchair.png'
+                        alt='armchair'
+                        sale='50% off'
+                />
+            </SwiperSlide>
+        </Swiper>
      );
 }
  
